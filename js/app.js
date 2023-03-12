08e33fed0973bdf400435788f0ffd2940decd421
HishamAtef55 <hishamatef50@gmail.com>
2023-03-12 21:39:45 +0200

    last update

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ let contents = document.querySelectorAll("section");
 // append li dynamic
 contents.forEach((element)=>{
   listItem = document.createElement("li");
-  listItem.innerHTML = `<li><a #${element.id} data-item="${element.id}" class="menu__link">${element.dataset.nav}</a></li>`;
+  listItem.innerHTML = `<li><a href="#${element.id}" data-item="${element.id}" class="menu__link">${element.dataset.nav}</a></li>`;
   navBar.appendChild(listItem); 
 })
 
@@ -17,33 +17,41 @@ window.onscroll = ClientRect;
 function ClientRect() {
   contents.forEach(function (element) {
     const  rect = element.getBoundingClientRect();
+
     if (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      element.getBoundingClientRect().top<window.innerHeight
     ) {
-      element.classList.add("your-active-class");
+      AddActiveClass(element.id)
+     
+     
     }
-    else {
-      element.classList.remove("your-active-class");
-    }
+   
   });
 };
 // when click on item from navigation menu the link should scroll to the appropriate section
 
 navBar.addEventListener("click", scrollToContent);
-function scrollToContent(e){ 
+function scrollToContent(e){
     e.preventDefault();
-  
     if (e.target.dataset.item) {
-     
       document
         .getElementById(`${e.target.dataset.item}`)
         .scrollIntoView({ behavior: "smooth" });
-        e.target.classList.add('active');
+       AddActiveClass(`${e.target.dataset.item}`)
       setTimeout(() => {
         location.hash = `${e.target.dataset.item}`;
       }, 300);
     }
+}
+function AddActiveClass(id){
+
+  // add li active
+  document.querySelector('.active')?.classList.remove('active');
+  document.querySelector(`[href='#${id}']`).classList.add('active');
+
+//  add active to section
+  document.querySelector(".your-active-class").classList.remove('your-active-class');
+    document.querySelector(`#${id}`).classList.add('your-active-class');
+
+
 }
